f3869d4254255e9278fae661207cc92ed74dcd37
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-08 22:28:14 +0400

    feat: getting data from my API, new type and component created

---
+++ src/types/mainPage.d.tsx
@@ -0,0 +1,11 @@
+
+
+export type MainPageType = {
+   currentPage: number,
+   logo:string,
+   postedAt:string,
+   position:string,
+   logoBackground:string,
+   company:string,
+   location:string
+}
